18b71349928b4033d303ef24b2ef4f231fe48c67
Nickock <=>
2025-10-30 00:25:46 -0300

    fuera carpeta dist

--- back/build/utils/validateSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateSchema = void 0;
-const validateSchema = (object, schema) => {
-    const result = schema.safeParse(object);
-    return result;
-};
-exports.validateSchema = validateSchema;
-//# sourceMappingURL=validateSchema.js.map
